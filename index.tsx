59c2d45115730d20171634fb6c643a1fc41bc636
Luca D'Amico <menteora@gmail.com>
2025-12-17 11:12:03 +0100

    feat: Simplify root rendering logic
    Remove hydrateRoot call and simplify the initial rendering setup. This change focuses on the `createRoot` API for initial client-side rendering.

--- index.tsx
+++ index.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import Home from './pages/Home';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 const container = document.getElementById('root');
-const app = (
-  <React.StrictMode>
-    <ThemeProvider>
-      <Home />
-    </ThemeProvider>
-  </React.StrictMode>
-);
 
-if (container && container.hasChildNodes()) {
-  hydrateRoot(container, app);
-} else if (container) {
-  createRoot(container).render(app);
+if (container) {
+  createRoot(container).render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <Home />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
 }
