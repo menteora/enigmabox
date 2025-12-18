be496fda8efb66ba6b39ea016d6c393ce103ff8c
menteora <menteora@users.noreply.github.com>
2025-12-18 08:10:29 +0000

    deploy: e997c9de3b111ecdd719616f22fb1f47e4e8172d

--- assets/chunks/chunk-BE8YhLwi.js
+++ assets/chunks/chunk-BE8YhLwi.js
@@ -1,4 +1,4 @@
-import{c as r}from"./chunk-DPUBU9vA.js";/**
+import{c as r}from"./chunk-byLoAgrx.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
