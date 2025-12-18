9a3162e9e5d5f4ab9a67183c1aded6059480c7a2
menteora <menteora@users.noreply.github.com>
2025-12-18 08:14:57 +0000

    deploy: d8b7c8370dbb63b64bd5e44748ce20ca5343880d

--- assets/chunks/chunk-Crpbmzh_.js
+++ assets/chunks/chunk-Crpbmzh_.js
@@ -1,4 +1,4 @@
-import{c as r}from"./chunk-byLoAgrx.js";/**
+import{c as r}from"./chunk-D7ky5ylL.js";/**
  * @license lucide-react v0.446.0 - ISC
  *
  * This source code is licensed under the ISC license.
